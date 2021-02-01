482203d7cd89aa4ba7d1d2adbb648e8ae8f06026
Hayden Young <hi@hbjy.dev>
2021-02-01 00:27:57 +0000

    feat: add pwa manifest customization

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,18 @@ module.exports = {
         siteUrl: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'http://localhost:8000'
     },
     plugins: [
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                name: "Rocky Linux Documentation",
+                short_name: "Rocky Docs",
+                start_url: "/",
+                background_color: "#18181b",
+                theme_color: "#10B981",
+                display: "standalone",
+                icon: "src/images/favicon.ico"
+            }
+        },
         "gatsby-plugin-postcss",
         "gatsby-plugin-sharp",
         "gatsby-plugin-react-helmet",
