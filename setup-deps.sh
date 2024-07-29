4e94d317d81e92b200d56c74273381fa5c4fab8d
Neil Hanlon <neil@rockylinux.org>
2024-07-29 11:33:44 -0400

    switch to use fastly compute actions

--- setup-deps.sh
+++ setup-deps.sh
@@ -15,15 +15,9 @@ pip install -r requirements.txt
 #   pip install "git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git"
 # fi
 
-test -d compute-js/bin || mkdir compute-js/bin
-test -x compute-js/bin/fastly || ( curl -L https://github.com/fastly/cli/releases/download/v10.12.3/fastly_v10.12.3_linux-amd64.tar.gz | tar -xzf /dev/stdin -C compute-js/bin/ )
-
 # mkdocs optimize plugin requires pngquant
 npm install pngquant
 
 # minify for reducing deployment size
+mkdir -p compute-js/bin
 test -x compute-js/bin/minify || (curl -L https://github.com/tdewolff/minify/releases/download/v2.20.18/minify_linux_amd64.tar.gz | tar -C compute-js/bin/ -xz minify)
-
-# jq (for yq)
-test -x compute-js/bin/jq || curl -L https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64 -o compute-js/bin/jq
-chmod +x compute-js/bin/jq
